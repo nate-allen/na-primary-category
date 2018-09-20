bf8add888ccfb575a12c288f6b1c7874d83c7d7e
Nathan Allen <nathan@fireflypartners.com>
2018-09-20 00:42:52 -0400

    Update plugin uri

--- na-primary-category.php
+++ na-primary-category.php
@@ -1,11 +1,11 @@
 <?php
 /*
 * Plugin Name: Primary Category
-* Plugin URI: https://github.com/nate-allen/primary-category
+* Plugin URI: https://github.com/nate-allen/na-primary-category
 * Description: Allows publishers to designate a primary category for posts, and query for posts by their primary category.
 * Version: 0.1.0
 * License: GPL-2.0+
-* Author URI: https://github.com/nate-allen/
+* Author URI: https://github.com/nate-allen
 * Text Domain: na-primary-category
 *
 * @package PrimaryCategory
