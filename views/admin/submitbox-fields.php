2ce8fca66087efa033553f604905bef1907d806c
Nathan Allen <nathan@fireflypartners.com>
2018-09-21 02:15:55 -0400

    Add logic for updating the primary category

--- views/admin/submitbox-fields.php
+++ views/admin/submitbox-fields.php
@@ -1,2 +1,2 @@
 <input type="hidden" class="na-primary-category-id" name="na_primary_category_id" value="<?php echo esc_attr( $primary_category ); ?>">
-<?php wp_nonce_field( "set_primary_category_{$post_id}", 'na_primary_category_nonce', false ); ?>
+<?php wp_nonce_field( "set_primary_category_{$post_id}", '_na_primary_category_nonce', false ); ?>
