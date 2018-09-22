ee274db230f4bb45591792317373b26a250fed4d
Nathan Allen <nathan@fireflypartners.com>
2018-09-22 13:06:13 -0400

    Clean up minor JavaScript formatting issues

--- src/js/admin/primary-category-admin.js
+++ src/js/admin/primary-category-admin.js
@@ -2,33 +2,32 @@
  * Admin functionality
  */
 
-"use strict";
+'use strict';
 
-let primary_category = {};
+let primaryCategory = {};
 
-primary_category.admin = function ($) {
+primaryCategory.admin = ( function( $ ) {
+
     // Private Variables
-    let $window = $(window),
-        $doc = $(document),
-        $body = $('body'),
+    let $body = $( 'body' ),
         $elements = {
-            category_checklist: $('#categorychecklist'),
-            hidden_field : $('.na-primary-category-id')
+            category_checklist: $( '#categorychecklist' ),
+            hidden_field: $( '.na-primary-category-id' )
         },
         self;
 
     return {
 
         // Kick everything off
-        init: function () {
-            self = primary_category.admin;
+        init: function() {
+            self = primaryCategory.admin;
 
             $body
                 .on( 'change', '#categorychecklist input[type=checkbox]', self.setup_buttons )
                 .on( 'click', '.na-make-primary-category', self.make_category_primary )
                 .on( 'change', '.yoast-wpseo-primary-term', self.update_hidden_field );
 
-            self.setup_primary_category();
+            self.setup_primaryCategory();
             self.maybe_setup_buttons();
         },
 
@@ -39,18 +38,19 @@ primary_category.admin = function ($) {
          * ensures that our plugin doesn't create unnecessary duplicate buttons
          */
         maybe_setup_buttons: function() {
+
             // If Yoast SEO is installed, we'll use their buttons
-            if ( window.primary_category_admin.yoast_seo_installed ) {
+            if ( window.primaryCategory_admin.yoast_seo_installed ) {
                 return;
             }
 
             // Don't show buttons if user doesn't have permission to set the primary category
-            if ( ! window.primary_category_admin.user_has_permission ) {
+            if ( ! window.primaryCategory_admin.user_has_permission ) {
                 return;
             }
 
             // Don't show buttons if there's only 1 category selected
-            if ( $elements.category_checklist.find('input[type=checkbox]:checked').length < 2 ) {
+            if ( $elements.category_checklist.find( 'input[type=checkbox]:checked' ).length < 2 ) {
                 return;
             }
 
@@ -62,17 +62,19 @@ primary_category.admin = function ($) {
          * Determines which categories need buttons added or removed
          */
         setup_buttons: function() {
+
             // If Yoast SEO is installed, we'll use their buttons
-            if ( window.primary_category_admin.yoast_seo_installed ) {
+            if ( window.primaryCategory_admin.yoast_seo_installed ) {
                 return;
             }
 
             // Loop over each category
-            $elements.category_checklist.find('li').each(function(){
-                let category = $(this).prop('id');
+            $elements.category_checklist.find( 'li' ).each( function() {
+                let category = $( this ).prop( 'id' );
 
                 // Don't add buttons to unchecked categories
-                if ( ! $(this).find('input[type=checkbox]').prop('checked') ) {
+                if ( ! $( this ).find( 'input[type=checkbox]' ).prop( 'checked' ) ) {
+
                     // Remove button if there was one
                     self.remove_make_primary_button( category );
 
@@ -84,7 +86,7 @@ primary_category.admin = function ($) {
                 }
 
                 // Add a "Primary" label if this is the primary category, otherwise add a "Make Primary" button
-                if ( $(this).hasClass( 'na-is-primary-category' ) ) {
+                if ( $( this ).hasClass( 'na-is-primary-category' ) ) {
                     self.add_primary_label( category );
                 } else {
                     self.add_make_primary_button( category );
@@ -92,41 +94,42 @@ primary_category.admin = function ($) {
             });
 
             // Get the first checked category
-            let category = $elements.category_checklist.find('input[type=checkbox]:checked:first').parents('li').prop('id');
+            let category = $elements.category_checklist.find( 'input[type=checkbox]:checked:first' ).parents( 'li' ).prop( 'id' );
 
             // Make sure at least one of the categories is the primary one
-            if ( ! $elements.category_checklist.find('.na-is-primary-category').length ) {
-                self.add_primary_label(category);
+            if ( ! $elements.category_checklist.find( '.na-is-primary-category' ).length ) {
+                self.add_primary_label( category );
             }
 
             // If there's only 1 category, remove primary label
-            if ( $elements.category_checklist.find('input[type=checkbox]:checked').length === 1 ) {
-                self.remove_primary_label(category);
+            if ( 1 === $elements.category_checklist.find( 'input[type=checkbox]:checked' ).length ) {
+                self.remove_primary_label( category );
             }
 
             // If there are no categories, remove primary category from hidden field
-            if ( ! $elements.category_checklist.find('input[type=checkbox]:checked').length ) {
-                $elements.hidden_field.val('');
+            if ( ! $elements.category_checklist.find( 'input[type=checkbox]:checked' ).length ) {
+                $elements.hidden_field.val( '' );
             }
         },
 
         /**
          * Set the primary category when the page loads
          */
-        setup_primary_category: function() {
-            // If Yoast SEO is installed, we'll use their primary category
-            if ( window.primary_category_admin.yoast_seo_installed ) {
-                let category_id = $('.yoast-wpseo-primary-term').val();
+        setup_primaryCategory: function() {
 
-                $elements.hidden_field.val( category_id );
+            // If Yoast SEO is installed, we'll use their primary category
+            if ( window.primaryCategory_admin.yoast_seo_installed ) {
+                let categoryID = $( '.yoast-wpseo-primary-term' ).val();
+
+                $elements.hidden_field.val( categoryID );
 
                 return;
             }
 
             if ( $elements.hidden_field.val() ) {
-                let category_id = $elements.hidden_field.val();
+                let categoryID = $elements.hidden_field.val();
 
-                self.add_primary_label( 'category-' + category_id );
+                self.add_primary_label( 'category-' + categoryID );
             }
         },
 
@@ -134,8 +137,9 @@ primary_category.admin = function ($) {
          * Marks a category as the primary one
          */
         make_category_primary: function() {
+
             // Reset primary category (in case there was one)
-            $elements.category_checklist.find('.na-is-primary-category').removeClass('na-is-primary-category');
+            $elements.category_checklist.find( '.na-is-primary-category' ).removeClass( 'na-is-primary-category' );
 
             // Add class to category for styling
             $( this ).parents( 'li' ).addClass( 'na-is-primary-category' );
@@ -150,10 +154,13 @@ primary_category.admin = function ($) {
          */
         add_make_primary_button: function( category ) {
             let $category = $( '#' + category );
-            let $button   = $('<button>', {type: 'button', class: 'na-make-primary-category'}).text(window.primary_category_admin.i18n.make_primary);
+            let $button   = $( '<button>', {
+                type: 'button',
+                class: 'na-make-primary-category'
+            } ).text( window.primaryCategory_admin.i18n.make_primary );
 
             // Don't add a button if there's already one
-            if ( $category.find('.na-make-primary-category').length ) {
+            if ( $category.find( '.na-make-primary-category' ).length ) {
                 return;
             }
 
@@ -171,10 +178,12 @@ primary_category.admin = function ($) {
          */
         add_primary_label: function( category ) {
             let $category   = $( '#' + category );
-            let $label      = $('<span>', {class: 'na-primary'}).text(window.primary_category_admin.i18n.primary);
+            let $label      = $( '<span>', {
+                class: 'na-primary'
+            } ).text( window.primaryCategory_admin.i18n.primary );
 
             // Skip if it already has a label
-            if ( $category.find('.na-primary').length ) {
+            if ( $category.find( '.na-primary' ).length ) {
                 return;
             }
 
@@ -185,9 +194,9 @@ primary_category.admin = function ($) {
 
             // Set the primary category id to the hidden field
             if ( category ) {
-                let category_id = category.match( /\d+/g );
+                let categoryID = category.match( /\d+/g );
 
-                $elements.hidden_field.val( category_id );
+                $elements.hidden_field.val( categoryID );
             }
         },
 
@@ -203,7 +212,7 @@ primary_category.admin = function ($) {
             $category.find( '.na-make-primary-category' ).remove();
 
             // If there's only 1 category checked, remove its button too
-            if ( $elements.category_checklist.find('input[type=checkbox]:checked').length < 2) {
+            if ( $elements.category_checklist.find( 'input[type=checkbox]:checked' ).length < 2 ) {
                 $elements.category_checklist.find( '.na-make-primary-category' ).remove();
             }
         },
@@ -220,10 +229,10 @@ primary_category.admin = function ($) {
             $category.find( '.na-primary' ).remove();
 
             // Remove class
-            if ( $category.hasClass('na-is-primary-category') ) {
+            if ( $category.hasClass( 'na-is-primary-category' ) ) {
                 $category.removeClass( 'na-is-primary-category' );
 
-                self.maybe_set_new_primary_category();
+                self.maybe_set_new_primaryCategory();
             }
         },
 
@@ -233,30 +242,31 @@ primary_category.admin = function ($) {
          * When a primary category is unchecked, we should set a new primary category unless
          * there are less than 2 categories selected
          */
-        maybe_set_new_primary_category: function() {
+        maybe_set_new_primaryCategory: function() {
+
             // Don't set a primary category if there aren't more than 1 category selected
-            if ( $elements.category_checklist.find('input[type=checkbox]:checked').length < 2 ) {
+            if ( $elements.category_checklist.find( 'input[type=checkbox]:checked' ).length < 2 ) {
                 return;
             }
 
             // Get the first checked category
-            let category = $elements.category_checklist.find('input[type=checkbox]:checked:first').parents('li').prop('id');
+            let category = $elements.category_checklist.find( 'input[type=checkbox]:checked:first' ).parents( 'li' ).prop( 'id' );
 
-            self.add_primary_label(category);
+            self.add_primary_label( category );
         },
 
         /**
          * If Yoast SEO is installed, update our hidden field when their primary category changes
          */
         update_hidden_field: function() {
-            let category_id = $('.yoast-wpseo-primary-term').val();
+            let categoryID = $( '.yoast-wpseo-primary-term' ).val();
 
-            $elements.hidden_field.val( category_id );
+            $elements.hidden_field.val( categoryID );
         }
 
     };
-}(jQuery);
+}( jQuery ) );
 
-jQuery(function ($) {
-    primary_category.admin.init();
+jQuery( function( $ ) {
+    primaryCategory.admin.init();
 });
